09db0055c08cb5f4cb9c8dfa4c4260da3c14af5e
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-05 20:31:59 +0500

    Property Search Box

---
+++ components/SearchProperty.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../styles/SearchProperty.module.css";
+
+const SearchProperty = () => {
+  return (
+    <div className="p-4 mb-8">
+      <div
+        className={`${styles.searchBox} bg-white p-10 rounded-lg search-property-box shadow-2xl shadow-slate-300 mx-40`}
+      >
+        hello
+      </div>
+    </div>
+  );
+};
+
+export default SearchProperty;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
+import SearchProperty from "../components/SearchProperty";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <meta name="description" content="Real Estate Site My Muhammad Uzair" />
       </Head>
       <Hero />
+      <SearchProperty />
     </div>
   );
 }
