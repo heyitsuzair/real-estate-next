f89711b6257fd80cf782eff9fe326e6e9eb0f762
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-06 19:56:58 +0500

    Scrollbar Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "../styles/Hero.css";
 import "../styles/Card.css";
+import "../styles/Scrollbar.css";
 import type { AppProps } from "next/app";
 import Topbar from "../components/common/Topbar";
 import Navbar from "../components/common/Navbar";
