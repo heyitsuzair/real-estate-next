377ed527f34ef7525728e1c2e03ff94206f5c573
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-14 19:55:06 +0500

    Fixes

--- yupSchemas/dashboard/AddPropertySchema.tsx
+++ yupSchemas/dashboard/AddPropertySchema.tsx
@@ -11,16 +11,16 @@ export const AddPropertySchema = Yup.object({
   type: Yup.string().required("Please Choose Property Type"),
   listing_media: Yup.array().min(1, "Please Upload Listing Media (Pictures)"),
   property_place_id: Yup.string().required("Please Enter Property Address"),
-  property_size: Yup.string().required("Please Enter Property Size"),
-  property_lot_size: Yup.string().required("Please Enter Property Lot Size"),
+  property_size: Yup.number().required("Please Enter Property Size"),
+  property_lot_size: Yup.number().required("Please Enter Property Lot Size"),
   property_rooms: Yup.number().required("Please Enter Total Rooms"),
   property_bed_rooms: Yup.number().required("Please Enter Total Bedrooms"),
   property_bath_rooms: Yup.number().required("Please Enter Total Bathrooms"),
   property_garages: Yup.number().required("Please Enter Total Garages"),
-  property_year_built: Yup.string().required(
+  property_year_built: Yup.number().required(
     "Please Enter Property Year Built"
   ),
-  property_garages_size: Yup.string().required(
+  property_garages_size: Yup.number().required(
     "Please Enter Property Garages Sizes"
   ),
   property_amenities: Yup.array().min(1, "Please Choose Amenities"),
