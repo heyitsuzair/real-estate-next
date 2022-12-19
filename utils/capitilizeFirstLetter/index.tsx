49d5f1f44962c4ce2e379389dbb521fc289867f5
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-19 23:19:27 +0500

    Final Pushes

--- utils/capitilizeFirstLetter/index.tsx
+++ utils/capitilizeFirstLetter/index.tsx
@@ -1,4 +1,4 @@
-export const capitalizeFirstLetter = (text: string | string[] | undefined) => {
+export const capitalizeFirstLetter = (text: any) => {
   if (typeof text !== "undefined") {
     const result = text.charAt(0).toUpperCase() + text.slice(1);
     return result;
